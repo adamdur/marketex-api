70aff2b8f1c7dda409fa1c7a499f178d9d519dde
aduris <duris@efabrica.sk>
2021-07-18 21:33:31 +0200

    logger add ip

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -2,7 +2,7 @@ import logger from '../../config/winston';
 
 export default (req, res, next) => {
     let requestBody = (typeof req.body !== 'string') ? JSON.stringify(req.body) : req.body;
-    logger.log('info', 'Method: ' + req.method + ' Path: ' + req.path + ' Request Body: ' + requestBody);
+    logger.log('info', 'Method: ' + req.method + ' Path: ' + req.path + ' IP: ' + req.connection.remoteAddress + ' Request Body: ' + requestBody);
     logger.log('info', '--------------------------------------------------');
     next();
 };
